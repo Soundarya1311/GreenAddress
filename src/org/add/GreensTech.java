89c6c1a3533d9f99ba32319eb443869e1ca8cdab
Soundarya1311 <soundaryakumar76@gmail.com>
2022-02-26 19:46:31 +0530

    folder structure

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,21 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+	
+		System.out.println("The Greens OMR branch");
+
+	}
+	private void greensAdayar() {
+		
+		System.out.println("The Greens Adayar branch");
+
+	}
+	public static void main(String[] args) {
+		
+		GreensTech gt = new GreensTech();
+		gt.greensAdayar();
+		gt.greensOmr();
+	}
+
+}
